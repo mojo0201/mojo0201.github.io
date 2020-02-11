2aa58692ffbb26f00f8d3fafad4bfcbaddd59011
mojo0201 <45469541+mojo0201@users.noreply.github.com>
2020-02-11 15:19:08 -0600

    image demo

--- image-demo/sketch.js
+++ image-demo/sketch.js
@@ -5,6 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let savage;
+
+function preload(){
+  savage = loadImage("assets/21savage.png")
+}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +18,7 @@ function setup() {
 
 function draw() {
   background(255);
-  fill(random(0,255),random(0,255),random(0,255),random(0,255))
-  rect(mouseX - 25, mouseY - 25, 50, 50)
+  // fill(random(0,255),random(0,255),random(0,255),random(0,255))
+  // rect(mouseX - 25, mouseY - 25, 50, 50)
+  image(savage, mouseX, mouseY, width -100, 100)  
 }
