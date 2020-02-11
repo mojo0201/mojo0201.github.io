6f7de44a647fa2a71be38a20a1e9f004e036bc05
mojo0201 <45469541+mojo0201@users.noreply.github.com>
2020-02-11 15:11:02 -0600

    new thing

---
+++ image-demo/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  fill(random(0,255),random(0,255),random(0,255),random(0,255))
+  rect(mouseX - 25, mouseY - 25, 50, 50)
+}
