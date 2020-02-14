64b2eda8c0bb8cdc9591576b2c7a48810364fb7f
mojo0201 <45469541+mojo0201@users.noreply.github.com>
2020-02-13 21:21:51 -0600

    simplifying rat movement and fixing boundrys

--- rpg/sketch.js
+++ rpg/sketch.js
@@ -10,6 +10,7 @@ let playerY = 256;
 let ratX = 384;
 let ratY = 384;
 let chance;
+let bgNum = 1;
 
 function preload() {
   bgOne = loadImage("assets/backroundOne.png");
@@ -20,34 +21,52 @@ function preload() {
 function setup() {
   createCanvas(512, 512);
   frameRate(30);
-  
 }
 
+function ratMovement() {
+  let chance = Math.ceil(random(0,4));
+  if (chance === 1) {
+    if(ratY > 32){
+      ratY = ratY - 32;
+    }
+  }
+  else if (chance === 2) {
+    if (ratY < 448) {
+      ratY = ratY + 32;    
+    }
+  }
+  else if (chance === 3) {
+    if (ratX < 480){
+      ratX = ratX + 32;
+    }
+  }
+  else if (chance === 4) {
+    if (ratX > 32){
+      ratX = ratX - 32;
+    }
+  }
+}
 
 function keyPressed() {
   if (keyCode === UP_ARROW || key === "w") {
     if (playerY > 32) {
       playerY = playerY - 32;  
     }
-
   }
   if (keyCode === DOWN_ARROW || key ==="s") {
-    if (playerY < 480) {
+    if (playerY < 448) {
       playerY = playerY + 32;
-    }
-    
+    } 
   }
   if (keyCode === RIGHT_ARROW || key === "d") {
     if (playerX < 480) {
       playerX = playerX + 32;
-    }
-        
+    }  
   }
   if (keyCode === LEFT_ARROW || key === "a") {
-    if (playerX > 0){
+    if (playerX > 32){
       playerX = playerX - 32;
     }
-    
   }
 }
 
@@ -57,54 +76,12 @@ function draw() {
   image(rat, ratX, ratY, 32, 32);
 
   if (ratX !== playerX && ratY !== playerY) {
-
-    let chance = Math.ceil(random(0,4));
-    if (chance === 1) {
-      if(ratY > 32){
-        ratY = ratY - 32;
-      }
-      else{
-        break
-      }
-    }
-    else if (chance === 2) {
-      ratY = ratY + 32;
-    }
-    else if (chance === 3) {
-      ratX = ratX + 32;
-    }
-    else if (chance === 4) {
-      ratX = ratX - 32;
-    }
+    ratMovement();
   }
   else if (ratX === playerX && ratY !== playerY) {
-    let chance = Math.ceil(random(0,4));
-    if (chance === 1) {
-      ratY = ratY - 32;
-    }
-    else if (chance === 2) {
-      ratY = ratY + 32;
-    }
-    else if (chance === 3) {
-      ratX = ratX + 32;
-    }
-    else if (chance === 4) {
-      ratX = ratX - 32;
-    }
+    ratMovement();
   }
   else if (ratX !== playerX && ratY === playerY) {
-    let chance = Math.ceil(random(0,4));
-    if (chance === 1) {
-      ratY = ratY - 32;
-    }
-    else if (chance === 2) {
-      ratY = ratY + 32;
-    }
-    else if (chance === 3) {
-      ratX = ratX + 32;
-    }
-    else if (chance === 4) {
-      ratX = ratX - 32;
-    }
+    ratMovement();
   }
 }
