623fcd72c559eabcfd8f35b8e15c934e56e63a04
mojo0201 <45469541+mojo0201@users.noreply.github.com>
2020-02-26 14:43:22 -0600

    movement

--- rpg/sketch.js
+++ rpg/sketch.js
@@ -13,7 +13,7 @@ let chance;
 let bgNum = 1;
 let period = 1000;
 let keyDownPeriod = 500;
-let keyTimeNow = 0;
+let keyTimeNow;
 let ratTimeNow = 0;
 
 function preload() {
@@ -28,49 +28,52 @@ function setup() {
 }
 
 function ratMovement() {
-  let chance = Math.ceil(random(0,4));
+  let chance = Math.ceil(random(0, 4));
   if (chance === 1) {
-    if(ratY > 32){
+    if (ratY > 32) {
       ratY = ratY - 32;
     }
   }
   else if (chance === 2) {
     if (ratY < 448) {
-      ratY = ratY + 32;    
+      ratY = ratY + 32;
     }
   }
   else if (chance === 3) {
-    if (ratX < 480){
+    if (ratX < 480) {
       ratX = ratX + 32;
     }
   }
   else if (chance === 4) {
-    if (ratX > 32){
+    if (ratX > 32) {
       ratX = ratX - 32;
     }
   }
 }
 
 function keyPressed() {
+  let keyTimeNow = millis();
   if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
     if (playerY > 32) {
-      if(millis() >= keyTimeNow + keyDownPeriod)
-        playerY -= 32;  
-        keyTimeNow = millis()
+      playerY -= 32;
+      if (millis() >= keyTimeNow + keyDownPeriod)
+        playerY -= 32;
+        keyTimeNow = millis();
+      console.log(millis(), keyTimeNow, keyDownPeriod);
     }
   }
   if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) {
     if (playerY < 448) {
       playerY += 32;
-    } 
+    }
   }
   if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
     if (playerX < 480) {
       playerX += 32;
-    }  
+    }
   }
   if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
-    if (playerX > 32){
+    if (playerX > 32) {
       playerX -= 32;
     }
   }
@@ -82,21 +85,21 @@ function draw() {
   image(rat, ratX, ratY, 32, 32);
 
   if (ratX !== playerX && ratY !== playerY) {
-    if(millis() >= ratTimeNow + period){
+    if (millis() >= ratTimeNow + period) {
       ratMovement();
       ratTimeNow = millis()
     }
   }
   else if (ratX === playerX && ratY !== playerY) {
-    if(millis() >= ratTimeNow + period){
-    ratMovement();
-    ratTimeNow = millis()
-   }
+    if (millis() >= ratTimeNow + period) {
+      ratMovement();
+      ratTimeNow = millis()
+    }
   }
   else if (ratX !== playerX && ratY === playerY) {
-    if(millis() >= ratTimeNow + period){
-    ratMovement();
-    ratTimeNow = millis()
+    if (millis() >= ratTimeNow + period) {
+      ratMovement();
+      ratTimeNow = millis()
     }
   }
 }
