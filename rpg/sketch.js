347d721e08fd637c904b4a52a3a0c92ab8a9b021
mojo0201 <45469541+mojo0201@users.noreply.github.com>
2020-02-28 15:20:19 -0600

    finished fixing movement began stats

--- rpg/sketch.js
+++ rpg/sketch.js
@@ -1,7 +1,26 @@
 // RPG
 // Seth Jones
 // 2/12/2020
-//
+
+//stats
+let illusion = 15;
+let conjuration = 15;
+let destruction = 15;
+let restoration = 15;
+let alteration = 15;
+let enchanting = 15;
+let smithing = 15;
+let heavyArmor = 15;
+let block = 15;
+let twoHanded = 15;
+let oneHanded = 15;
+let archery = 15;
+let lightArmor = 15;
+let sneak = 15;
+let lockpicking = 15;
+let pickpocket = 15;
+let speech = 15;
+let alchemy = 15;
 
 let bgOne;
 let player;
@@ -11,15 +30,19 @@ let ratX = 384;
 let ratY = 384;
 let chance;
 let bgNum = 1;
-let period = 1000;
-let keyDownPeriod = 500;
+let ratPeriod = 1000;
+let keyDownPeriod = 250;
 let keyTimeNow;
+
+let rat;
 let ratTimeNow = 0;
+
 let up = false;
 let down = false;
 let right = false;
 let left = false;
 let state;
+
 
 
 function preload() {
@@ -58,31 +81,40 @@ function ratMovement() {
 }
 
 function keyPressed() {
+
+
+
+
+  //Movement Keys
   if (keyCode === UP_ARROW || key === "w") {
     if (playerY > 32) {
       up = true;
+      keyTimeNow = millis();
     }
   }
   if (keyCode === DOWN_ARROW || key === "s") {
     if (playerY < 448) {
       down = true;
+      keyTimeNow = millis();
     }
   }
   if (keyCode === RIGHT_ARROW || key === "d") {
     if (playerX < 480) {
       right = true;
+      keyTimeNow = millis();
     }
   }
   if (keyCode === LEFT_ARROW || key === "a") {
     if (playerX > 32) {
       left = true;
+      keyTimeNow = millis();
     }
   }
 }
 
 function keyReleased() {
   if (keyCode === UP_ARROW || key === "w") {
-      up = false;
+    up = false;
   }
   if (keyCode === DOWN_ARROW || key === "s") {
     down = false;
@@ -91,43 +123,49 @@ function keyReleased() {
     right = false;
   }
   if (keyCode === LEFT_ARROW || key === "a") {
-      left = false;
+    left = false;
   }
 }
 
 function movement() {
   if (playerX === ratX && playerY === ratY) {
-    state = "battle"
+    state = "battle";
+    up = false;
+    down = false;
+    right = false;
+    left = false;
+
   }
   if (up === true && millis() >= keyTimeNow + keyDownPeriod) {
-    keyTimeNow = millis();
     if (playerY > 32) {
       playerY -= 32;
       keyTimeNow = millis();
     }
   }
   if (down === true && millis() >= keyTimeNow + keyDownPeriod) {
-    keyTimeNow = millis();
     if (playerY < 448) {
       playerY += 32;
       keyTimeNow = millis();
     }
   }
   if (right === true && millis() >= keyTimeNow + keyDownPeriod) {
-    keyTimeNow = millis();
     if (playerX < 480) {
       playerX += 32;
       keyTimeNow = millis();
     }
   }
   if (left === true && millis() >= keyTimeNow + keyDownPeriod) {
-    keyTimeNow = millis();
     if (playerX > 32) {
       playerX -= 32;
       keyTimeNow = millis();
     }
   }
 }
+
+// function states(){
+//   if (state === "battle") {
+//   }
+// }
 
 
 function draw() {
@@ -137,21 +175,21 @@ function draw() {
   image(rat, ratX, ratY, 32, 32);
 
   if (ratX !== playerX && ratY !== playerY) {
-    if (millis() >= ratTimeNow + period) {
+    if (millis() >= ratTimeNow + ratPeriod) {
       ratMovement();
-      ratTimeNow = millis()
+      ratTimeNow = millis();
     }
   }
   else if (ratX === playerX && ratY !== playerY) {
-    if (millis() >= ratTimeNow + period) {
+    if (millis() >= ratTimeNow + ratPeriod) {
       ratMovement();
-      ratTimeNow = millis()
+      ratTimeNow = millis();
     }
   }
   else if (ratX !== playerX && ratY === playerY) {
-    if (millis() >= ratTimeNow + period) {
+    if (millis() >= ratTimeNow + ratPeriod) {
       ratMovement();
-      ratTimeNow = millis()
+      ratTimeNow = millis();
     }
   }
 }
