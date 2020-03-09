db03c746985b2b6c4d1abd6644ae4506c10249a8
mojo0201 <45469541+mojo0201@users.noreply.github.com>
2020-03-09 15:15:07 -0600

    new stuff

---
+++ newthing/sketch.js
@@ -0,0 +1,27 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let cellSize = 20;
+isBlack = true;
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      if (isBlack)
+        fill("black");
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+  else {
+        fill("white")
+      }
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      isBlack != isBlack;
+    }
+    isBlack != isBlack
+  }
+}
